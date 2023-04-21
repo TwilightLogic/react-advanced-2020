ed3afeadb766ae1cce88e49146da0f0e51e54ba1
TwilightLogic <twilightlogic@gmail.com>
2023-04-21 10:12:31 +0800

    Update `useEffect` notes

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -15,6 +15,8 @@ const UseEffectCleanup = () => {
 
     // If we don't use cleanup function.
     // We will see bunch of event listeners in 'Elements' of out browser.
+    // Cleanup logic should be “symmetrical” to the setup logic (`window.addEventListener` here)
+    // , and should stop or undo whatever setup did:
     return () => {
       window.removeEventListener('resize', checkSize);
     };
